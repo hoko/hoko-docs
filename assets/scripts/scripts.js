37d65208bd21501caedae5664ef49e11fe60768c
Vasco Gaspar <vvascogasparr@gmail.com>
2015-10-27 14:39:17 +0000

    Added anchor link to headings

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,13 @@
 $(function() {
   $('.language-swift').closest('.highlight').hide();
 
-  $('.language-objective-c').closest('.highlight').before('<div class="toggle-container"><span class="toggle-label  on">Objective-C</span><div class="flat-toggle"></div><span class="toggle-label last">Swift</span></div>');
+  $('.language-objective-c').closest('.highlight').before('<div class="toggle-container"><span class="toggle-label on">Objective-C</span><div class="flat-toggle"></div><span class="toggle-label last">Swift</span></div>');
 
   $('.flat-toggle, .toggle-label').on('click', function() {
-    $('.flat-toggle, .toggle-label').toggleClass('on');
-
     var highlightOC = $('.language-objective-c').closest('.highlight');
     var highlightSwift = $('.language-swift').closest('.highlight');
+
+    $('.flat-toggle, .toggle-label').toggleClass('on');
 
     if ($('.flat-toggle').hasClass('on')) {
       highlightOC.hide();
@@ -18,13 +18,21 @@ $(function() {
     }
   });
 
+  // Get latest version from iOS SDK and update wherever needed
   $('.ios-version').load('https://hokolinks.com/versions/ios', function(data) {} );
 
+  // Get latest version from Android SDK and update wherever needed
   $('.android-version').load('https://hokolinks.com/versions/android', function(data) {
     var av = $('.android-version:first').text();
-
-    $('#aar-download').attr("href", 'https://oss.sonatype.org/service/local/repositories/releases/content/com/hokolinks/hoko/' + av +'/hoko-' + av +'.aar')
+    $('#aar-download').attr("href", 'https://oss.sonatype.org/service/local/repositories/releases/content/com/hokolinks/hoko/' + av +'/hoko-' + av +'.aar');
   } );
+
+  // Add anchor link to each h2 & h3 heading
+  $('h3, h2').each(function() {
+    var id = $(this).attr('id');
+    var anchor = '<a href="#' + id + '" class="anchor"><span class="octicon octicon-link"></span></a>';
+    $(this).prepend(anchor);
+  });
 
 
 });
